8d994417aca9e9c6ef168eb8c9282914ee010ed4
kemechial <kaanevran@gmail.com>
2024-11-28 07:18:57 +0300

    Creating route and playing sound in browser.

--- firstapp/src/Header.js
+++ firstapp/src/Header.js
@@ -1,6 +1,16 @@
+import logo from "./assets/logo.png"
 
 function Header() {
-    return <h1>Hello World!</h1>;
+    return (     
+      <div>
+         <h1>Hello World!</h1>
+         <img src={logo} alt="Logo"
+          height={200}
+         />
+     </div>
+
+    );
+
   }
 
 export default Header;
